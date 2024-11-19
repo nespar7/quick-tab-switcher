705fd3cdf0944929a203489cd9dc757146ca6e90
nespar7 <nsuryaprakashreddy1234@gmail.com>
2024-11-20 00:43:49 +0530

    Added attach and detach tab functionality

--- background.js
+++ background.js
@@ -5,40 +5,7 @@ let currentWindowId = null;
 let openTabs = [];
 let tabPointer = 0;
 
-<<<<<<< Updated upstream
-function loadTabsForCurrentWindow() {
-    // chrome.tabs.query({ currentWindow: true }, (tabs) => {
-=======
 function loadWindowTabs() {
-    // chrome.tabs.query({}, (tabs) => {
->>>>>>> Stashed changes
-    //     const activeTab = tabs.find(tab => tab.active);
-    //     if(activeTab) {
-    //         openTabs = [activeTab, ...tabs.filter(tab => tab.id !== activeTab.id)]
-    //                     .map(tab => {
-    //                         return {
-    //                             id: tab.id,
-    //                             title: tab.title,
-    //                             url: tab.url
-    //                         }
-    //                     });
-    //     }
-    //     else {
-    //         openTabs = tabs.map(tab => {
-    //             return {
-    //                 id: tab.id,
-    //                 title: tab.title,
-    //                 url: tab.url
-    //             }
-    //         });
-    //     }
-    //     console.log("Tabs loaded", openTabs);
-    // });
-<<<<<<< Updated upstream
-
-    chrome.windows.getCurrent({populate: true}, (window) => {
-        currentWindowId = currentWindow.id;
-=======
     chrome.windows.getCurrent({populate: true}, (window) => {
         currentWindowId = window.id;
         openTabs = window.tabs.map(tab => {
@@ -49,7 +16,6 @@ function loadWindowTabs() {
             }
         });
         console.log(`Tabs loaded for window ${currentWindowId}: `, openTabs);
->>>>>>> Stashed changes
     });
 }
 
@@ -58,53 +24,16 @@ function setActiveTab(tabId) {
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-<<<<<<< Updated upstream
-    loadTabsForCurrentWindow();    
-=======
     loadWindowTabs();    
->>>>>>> Stashed changes
     console.log("Extension installed with tabs: ", openTabs);
 });
 
 chrome.runtime.onStartup.addListener(() => {
-<<<<<<< Updated upstream
-    loadTabsForCurrentWindow();    
-=======
     loadWindowTabs();    
->>>>>>> Stashed changes
     console.log("Browser started with tabs: ", openTabs);
 });
 
 chrome.tabs.onCreated.addListener((tab) => {
-<<<<<<< Updated upstream
-    chrome.windows.getCurrent((window) => {
-        if(tab.windowId === window.id) {
-            openTabs.unshift({
-                id: tab.id,
-                title: tab.title,
-                url: tab.url
-            });
-            console.log(`Tab ${tab.id} created in window ${window.id}`);
-        }
-    });
-});
-
-chrome.tabs.onAttached.addListener((tabId, attachInfo) => {
-    chrome.tabs.get(tabId, (tab) => {
-        if(tab.windowId === attachInfo.newWindowId) {
-            openTabs.unshift({
-                id: tab.id,
-                title: tab.title,
-                url: tab.url
-            });
-        }
-    });
-});
-
-chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
-    openTabs = openTabs.filter(tab => tab.id !== tabId);
-    console.log("Tab detached: ", tabId);
-=======
     if(tab.windowId === currentWindowId) {
         console.log("Tab created: ", tab);
         openTabs.unshift({
@@ -113,7 +42,6 @@ chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
             url: tab.url
         })
     }
->>>>>>> Stashed changes
     console.log("Open tabs: ", openTabs);
 });
 
@@ -123,11 +51,6 @@ chrome.tabs.onRemoved.addListener((tabId) => {
     console.log("Open tabs: ", openTabs);
 });
 
-<<<<<<< Updated upstream
-chrome.tabs.onActivated.addListener((activeInfo) => {
-    console.log("Activated tab: ", activeTabId);
-    console.log("Open tabs: ", openTabs);
-=======
 chrome.tabs.onActivated.addListener(({tabId, windowId}) => {
     if(windowId === currentWindowId) {
         activeTabId = tabId;
@@ -141,7 +64,6 @@ chrome.tabs.onActivated.addListener(({tabId, windowId}) => {
             }
         }
     }
->>>>>>> Stashed changes
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
@@ -161,6 +83,28 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 });
 
+chrome.tabs.onDetached.addListener((tabId, detachInfo) => {
+    if(detachInfo.oldWindowId === currentWindowId) {
+        openTabs = openTabs.filter(tab => tab.id !== tabId);
+        console.log("Tab detached: ", tabId);
+        console.log("Open tabs: ", openTabs);
+    }
+});
+
+chrome.tabs.onAttached.addListener((tabId, attachInfo) => {
+    if(attachInfo.newWindowId === currentWindowId) {
+        chrome.tabs.get(tabId, (tab) => {
+            openTabs.unshift({
+                id: tab.id,
+                title: tab.title,
+                url: tab.url
+            });
+            console.log("Tab attached: ", tab);
+            console.log("Open tabs: ", openTabs);
+        });
+    }
+});
+
 chrome.commands.onCommand.addListener((command) => {
     console.log("Command received: ", command);
     if(command === "most_recent") {
@@ -171,7 +115,6 @@ chrome.commands.onCommand.addListener((command) => {
     }
 });
 
-<<<<<<< Updated upstream
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if(message.action === "get_tabs") {
         console.log(message)
@@ -182,16 +125,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     } else if (message.action === "received_tabs") {
         console.log("Received tabs: ", message.tabs);
         sendResponse({ success: true });
-=======
-chrome.runtime.onMessage.addListener((message) => {
-    if(message.action === "get_tabs") {
-        console.log(message)
-        return openTabs;
-    } else if (message.action === "set_active_tab" && message.tabId) {
-        chrome.tabs.update(message.tabId, { active: true });
-    } else if (message.action === "recieved_tabs") {
-        console.log("Recieved tab: ", message);
->>>>>>> Stashed changes
     }
 
     return true;
